4289d93e591a22d63e07798e8d5bff48bc81525d
ironowl-txw <tie.xiao.wang@gmail.com>
2020-10-17 22:46:06 -0700

    restored content.js

--- extension/content.js
+++ extension/content.js
@@ -1,11 +1,13 @@
-console.log("loaded")
-let item = id("productTitle").textContent.trim()
+// The code for the EcoEats extension. Runs in Amazon product pages.
+const BASE_URL = "https://caaatdubhacks.pythonanywhere.com/"
 
+let item = encodeURI(id("productTitle").textContent.trim())
 let aboutList = qs("#feature-bullets > ul")
+let ingredient = encodeURI(qs("#important-information > div").textContent.trim().substring(11).trim())
 
-let ingredient = qs("#important-information > div").textContent.trim().substring(11).trim()
 
-fetch("https://caaatdubhacks.pythonanywhere.com/api")
+
+fetch(BASE_URL + "endpoint")
   .then(checkStatus)
   .then(JSON.parse)
   .then(displayData)
@@ -21,17 +23,15 @@ function checkStatus(response) {
 }
 
 function displayData(responseData) {
-  responseData["total emissions with eutrophying"] = responseData["total emissions with eutrophying"].toFixed(2)
+  responseData["total emissions with eutrophying"] = responseData["total emissions with eutrophying"].toFixed(2);
+  sessionStorage.setItem("response", responseData);
   let ecoEffect = document.createElement("li");
-  if(responseData.title_used == true){
-    ecoEffect.innerHTML = "<a href=\"" + "https://caaatdubhacks.pythonanywhere.com/" +
-     "\"><b>EcoEats Info:</b> " + responseData["food_matched"] + " emits " +
-     responseData["total emissions with eutrophying"] + "kilograms of CO-2 equivalent per kilogram of product" + "<a>"
-  } else {
-    ecoEffect.innerHTML = "<a href=\"" + "https://caaatdubhacks.pythonanywhere.com/" +
-     "\"><b>EcoEats Info:</b> " + "Unable to find data for this specific product, but main ingredient " + responseData["food_matched"] + " emits " +
-     responseData["total emissions with eutrophying"] + "kilograms of CO-2 equivalent per kilogram of product" + "<a>"
+  ecoEffect.innerHTML = "<a href=\"" + "https://caaatdubhacks.pythonanywhere.com/table" + "\"><b>EcoEats Info:</b></a> ";
+  if(responseData.title_used == false){
+    ecoEffct.innerHTML += + "Unable to find data for this specific product, but main ingredient ";
   }
+  ecoEffect.innerHTML += responseData["food_matched"] + " emits " +
+     responseData["total emissions with eutrophying"] + " kilograms of CO-2 equivalent per kilogram of product";
   aboutList.appendChild(ecoEffect)
 }
 
@@ -42,3 +42,98 @@ function id(idName) {
 function qs(query) {
   return document.querySelector(query);
 }
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Sustainibilty Scanner</title>
+    <style>
+      body {background-color: MediumSpringGreen;}
+      h2   {color: white; text-align: center;}
+      h3   {color: white; text-align: center;}
+      div {display: flex; justify-content: center;}
+      button {background-color: DeepSkyBlue; color: white; border-color: white; border-radius: 8px;  border-style: solid; text-bold: yes;}
+      /* The switch - the box around the slider */
+      .switch {
+        position: relative;
+        display: inline-block;
+        width: 60px;
+        height: 34px;
+      }
+
+      /* Hide default HTML checkbox */
+      .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+      }
+
+      /* The slider */
+      .slider {
+        position: absolute;
+        cursor: pointer;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #ccc;
+        -webkit-transition: .4s;
+        transition: .4s;
+      }
+
+      .slider:before {
+        position: absolute;
+        content: "";
+        height: 26px;
+        width: 26px;
+        left: 4px;
+        bottom: 4px;
+        background-color: white;
+        -webkit-transition: .4s;
+        transition: .4s;
+      }
+
+      input:checked + .slider {
+        background-color: #2196F3;
+      }
+
+      input:focus + .slider {
+        box-shadow: 0 0 1px #2196F3;
+      }
+
+      input:checked + .slider:before {
+        -webkit-transform: translateX(26px);
+        -ms-transform: translateX(26px);
+        transform: translateX(26px);
+      }
+
+      /* Rounded sliders */
+      .slider.round {
+        border-radius: 34px;
+      }
+
+      .slider.round:before {
+        border-radius: 50%;
+      }
+    </style>
+  </head>
+  <body>
+      <img src="earth.png" width="100" height="100">
+      <div>
+      <a href="https://caaatdubhacks.pythonanywhere.com/table", target="blank">
+        <button>
+          <b>View Detailed Breakdown</b>
+        </button>
+      </a>
+    </div>
+    <!-- <h3>Turn On</h3>
+    <div>
+      <label  class="switch">
+        <input type="checkbox">
+        <span class="slider round"></span>
+      </label>
+    </div> -->
+  </body>
+</html>
